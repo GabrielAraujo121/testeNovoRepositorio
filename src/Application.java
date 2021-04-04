430cd2555e952aa46afdc3337a4fdc8e78e21256
Gabriel Araujo <gabriel-araujo1020@hotmail.com>
2021-04-04 16:21:22 -0300

    Informando o nome do usuário

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,21 @@
-
+import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
 		//Apenas um teste
+
+		
+		Scanner ler = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Informe seu nome: ");
+		String nome = ler.nextLine();
+		System.out.println();
+		System.out.println("Olá " + nome);
+		
+		
+	
 	}
 
 }
